49043f713931d37e32c68324db16b3630445d301
Danielv123 <tranen.bananen@gmail.com>
2017-02-27 15:07:45 +0100

    Go app connects to #factorio and prints all chat to stdout

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	irc "github.com/fluffle/goirc/client"
 )
 
 func main() {
-  fmt.Println("IRC bridge initialized") 
+	fmt.Println("IRC bridge initialized")
+
+	// Creating a simple IRC client is simple.
+	c := irc.SimpleClient("clusterioBridge")
+
+	// Add handlers to do things here!
+	// e.g. join a channel on connect.
+	c.HandleFunc(irc.CONNECTED,
+		func(conn *irc.Conn, line *irc.Line) { conn.Join("#factorio") })
+	// And a signal on disconnect
+	quit := make(chan bool)
+	c.HandleFunc(irc.DISCONNECTED,
+		func(conn *irc.Conn, line *irc.Line) { quit <- true })
+
+	// handle people typing things
+	c.HandleFunc("privmsg",
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("/c game.print( \\'" + line.Nick + " | " + line.Args[1] + "\\' )")
+		})
+	// ... or, use ConnectTo instead.
+	if err := c.ConnectTo("ipo.esper.net"); err != nil {
+		fmt.Printf("Connection error: %s\n", err.Error())
+	}
+
+	// Wait for disconnect
+	<-quit
 }
